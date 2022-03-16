43d6fc8d10852b3a03b820c3f4c2dbce6a4d0448
Lutas <Fanateshen@gmail.com>
2022-03-16 21:43:54 +0300

    Add left/right button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +17,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
+        // Вызывает управление кнопкой из Input Manager
+        horizontalInput = Input.GetAxis("Horizontal");
+
         //transform.Translate(0, 0, 1);
+
+        // Move the vehicle forward
+        // deltaTime меняет action per frame на action per second, т.к. разные девайсы выдают разный фрейм
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
